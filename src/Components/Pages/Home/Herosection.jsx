68d8b50d24bf296aeb7135bf861577314f97ea04
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-05-31 01:11:26 +0600

    footer done

--- src/Components/Pages/Home/Herosection.jsx
+++ src/Components/Pages/Home/Herosection.jsx
@@ -3,7 +3,7 @@ import bgimage from "../../../assets/hero-img.png";
 
 const Herosection = () => {
   return (
-    <div className="absolute top-[-110px] w-full  mx-auto bg-bg-hero bg-cover h-[740px] bg-no-repeat  flex items-center pl-[330px]">
+    <div className="absolute top-[-110px] w-full  mx-auto bg-bg-hero bg-cover h-[740px] bg-no-repeat  flex items-center pl-[330px] hidden">
       <div className="">
         <h1 className="font-black text-7xl lg:w-[700px] text-white leading-12">
           Vecchio Evokes the Generosity, Humor & Simiplicity of Italy.
@@ -15,8 +15,8 @@ const Herosection = () => {
         </p>
         <div>
           <a href="#_" className="relative inline-block text-lg group mt-6">
-            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-primary rounded-lg group-hover:text-white">
-              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-primary"></span>
+            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out border-2 border-accent  group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3  bg-primary"></span>
               <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-accent group-hover:-rotate-180 ease "></span>
               <span className="relative text-xl font-bold ">Book a Table</span>
             </span>
--- src/Components/shared/Footer/Footer.jsx
+++ src/Components/shared/Footer/Footer.jsx
@@ -1,33 +1,71 @@
 import React from 'react';
 import logo from "../../../assets/logo-2.png";
+import img1 from "../../../assets/img-1.png"
+import img2 from "../../../assets/img-2.png";
+import img3 from "../../../assets/img-3.png";
+import img4 from "../../../assets/img-4.png";
+import img5 from "../../../assets/img-5.png";
+import img6 from "../../../assets/img-6.png";
+import { FaBeer } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-bg-nav min-h-[262px] object-cover ">
-        <div className="grid lg:grid-cols-3">
-          <div className=" w-[1028px] m-auto">
+    <div className='z'>
+      <div className="bg-bg-nav min-h-[262px] py-20 ">
+        <div className="grid lg:grid-cols-3 text-white text-center w-3/5 mx-auto gap-8 ">
+          <div className="  flex flex-col items-center">
             <img src={logo} alt="" className=" lg:w-[200px]" />
-            <p>
+            <p className="text-accent text-lg  font-bold leading-6 mt-6">
               Experience the true essence of Neapolitan pizzas cooked in a
               wood-fired oven, crafted with utmost reverence for tradition.
               Indulge in our delectable, handcrafted pasta, including a must-try
-              truffle-infused variety. Our extensive selection boasts over a
-              hundred exceptional wines from renowned vineyards. And to conclude
-              your meal on a sweet note, savor our delightful Italian Gelato.
+              truffle-infused variety. 
+              <br /> <br />
+              And to conclude your meal on a sweet note, savor our delightful
+              Italian Gelato.
             </p>
           </div>
 
           <div>
-            <h2> We are on Instagram</h2>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
+            <h2 className="text-white text-3xl font-bold">
+              {" "}
+              We are on Instagram
+            </h2>
+            <div className="h-[2px] px-2 w-[80px] mt-4 bg-accent mx-auto"></div>
+            <div className="h-[2px] px-2 w-[50px] mt-1 bg-accent mx-auto mb-4"></div>
+            <div className="grid grid-cols-3 w-5/6 gap-2 mx-auto justify-center">
+              <span >
+                <img src={img1} alt="" />
+              </span>
+              <span>
+                <img src={img2} alt=""  />
+              </span>
+              <span>
+                <img src={img3} alt="" />
+              </span>
+              <span>
+                <img src={img4} alt=""  />
+              </span>
+              <span>
+                <img src={img5} alt=""  />
+              </span>
+              <span>
+                <img src={img6} alt="" />
+              </span>
             </div>
+          </div>
+          <div className=" text-center">
+            <h2 className="text-white text-3xl font-bold">Get in Touch</h2>
+            <div className="h-[2px] px-2 w-[80px] mt-4 bg-accent mx-auto"></div>
+            <div className="h-[2px] px-2 w-[50px] mt-1 bg-accent mx-auto mb-8"></div>
+
+            <p className="text-accent font-bold text-xl">
+              Stay Tuned with Our Updates!
+            </p>
+           <input type="text" placeholder='Enter Your Email' className='px-8 py-3 mt-4 text-center bg-white text-xl' />
+           <button className='text-accent text-2xl border-b-2 border-accent block  mt-4 mx-auto hover:text-white hover:border-white 
+           '>Subscribe</button>
           </div>
         </div>
       </div>
